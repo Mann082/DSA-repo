961da18ef0d2649a93a04d4bb78a94b09eeb79d8
Mann082 <guptamann108@gmail.com>
2022-12-01 19:06:10 +0530

    1-12-22

---
+++ stack/postfix_evaluation.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<stack>
+#include<cmath>
+using namespace std;
+bool operatorr(char c){
+    if(c=='+' || c=='-' || c=='*' || c=='/' || c=='^' || c=='%')
+    return true;
+    return false;
+}
+char solve(char c,int a,int b){
+    int m=a,n=b,x;
+    switch(c){
+        case '+': x=m+n;
+        break;
+        case '-': x=m-n;
+        break;
+        case '/': x=m/n;
+        break;
+        case '*': x=m*n;
+        break;
+        case '^': x=pow(m,n);
+        break;
+        case '%': x=m%n;
+        break; 
+    }
+    return x;
+}
+void evaluate(string s){
+    stack<int> st;
+    for(int i=0;i<s.size();i++){
+        // cout<<st.top()<<" ";
+        if(operatorr(s[i])){
+            int operand1=st.top();
+            st.pop();
+            int operand2=st.top();
+            st.pop();
+            st.push(solve(s[i],operand2,operand1));
+            continue; 
+        }
+        st.push(s[i]-'0');
+    }
+    cout<<st.top()<<endl;
+}
+int main(){
+    string s;
+    cout<<"Enter the postfix expresion:"<<endl;
+    cin>>s;
+    cout<<"The Evaluated value is:"<<endl;
+    evaluate(s);
+    return 0;
+}
